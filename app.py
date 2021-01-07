1735434b8ca2898e8c19cc6b352f187649b6f353
kwalish <fabien.martin87@gmail.com>
2021-01-07 10:49:12 +0100

    Ethan Update

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import uuid
 import io
 from dotenv import load_dotenv
 from keras_vggface import VGGFace
+from multiprocessing import Process
 
 from utils import match_faces, get_VIPs, Minio, Connection, crop_faces
 
 load_dotenv()
 
-# instanciate db and Minio
-minioClient = Minio()
+# instanciate db
 Conn = Connection()
 Embedding, Frame, Face, VIP = Conn.get_tables()
 session = Conn.get_session()
@@ -20,37 +20,40 @@ session = Conn.get_session()
 # retrieve VIPs embeddings
 vip_ids, vip_embeds = get_VIPs(session, Embedding, VIP)
 
-
-
-
-print('[debug] Starting Server')
-image_hub = imagezmq.ImageHub(open_port='tcp://192.168.1.27:5556', REQ_REP=False)
-
-while True:
-
-    # receive the frame
-    data, frame = image_hub.recv_image()
-
-    # prepare frame for storage
-    tick = datetime.now()
-    frame_string = cv2.imencode('.jpg', frame)[1].tostring()
+def create_image_info(camera_id):
     frame_id = str(uuid.uuid1())
-    url_key = "camera_{}/{}.jpg".format(data["camera_id"], frame_id)
+    url_key = "camera_{}/{}.jpg".format(camera_id, frame_id)
     url = "http://15.188.86.39:9001/organization1/{}".format(url_key)
+    return frame_id, url, url_key
+
+def send_image_minio(frame, url_key):
+    tick = datetime.now()
+    minioClient = Minio()
+    frame_string = cv2.imencode('.jpg', frame)[1].tostring()
     image_stream = io.BytesIO(frame_string)
     minioClient.save_frame("organization1", url_key, image_stream, len(frame_string))
     print("[debug] saving frame takes {}".format(datetime.now() - tick))
 
-    # extract embeds from data
-    boxes = data["bbox"]
 
-    tack = datetime.now()
-    if len(boxes):
-        # load embedder
-        embedder = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-        faces = crop_faces(frame, boxes)
-        embeds = embedder.predict(faces).tolist()
-    print("[debug] facial feature extraction takes {}".format(datetime.now() - tack))
+
+print('[debug] Starting Server')
+image_hub = imagezmq.ImageHub(open_port='tcp://192.168.1.31:5555', REQ_REP=False)
+
+embedder = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+
+count = 0
+
+while True:
+    tick = datetime.now()
+    # receive the frame
+    data, frame = image_hub.recv_image()
+
+    # prepare frame for storage
+    frame_id, url, url_key = create_image_info(data["camera_id"])
+    p = Process(target=send_image_minio, args=(frame, url_key,))
+    p.start()
+    # p.join()
+    # extract embeds from data
 
     # save the frame information in the database
     session.execute(Frame.insert().values(
@@ -60,27 +63,42 @@ while True:
         timestamp=data["timestamp"]
     ))
 
-    # if len(embeds):
-    #     embeds = np.array(embeds)
-    #     print(embeds.shape)
-    #     print(vip_embeds.shape)
-    #     matches = match_faces(embeds, vip_embeds, threshold=.55)
-    #
-    #     for i, index in enumerate(matches):
-    #         bounding_box = data["bbox"][i]
-    #         VIP_id = 16
-    #         if not index == -1:
-    #             VIP_id = vip_ids[index]
-    #
-    #         # save the face information in the database
-    #         session.execute(Face.insert().values(
-    #             frame_id=frame_id,
-    #             VIP_id=VIP_id,
-    #             bounding_box=bounding_box
-    #         ))
+    boxes = data["bbox"]
+
+
+    if len(boxes) > 0:
+        tack = datetime.now()
+        # load embedder
+        faces = crop_faces(frame, boxes)
+        embeds = embedder.predict(faces).tolist()
+        print("[debug] facial feature extraction takes {}".format(datetime.now() - tack))
+        if len(embeds):
+            embeds = np.array(embeds)
+            matches = match_faces(embeds, vip_embeds, threshold=.55)
+            print("---MATCHES---")
+            print(len(matches))
+            print(matches)
+            print("---BBOX---")
+            print(len(data["bbox"]))
+            print(data["bbox"])
+            for i, index in enumerate(matches):
+                bounding_box = data["bbox"][i]
+                VIP_id = 16
+                if not index == -1:
+                    VIP_id = vip_ids[index]
+
+                # save the face information in the database
+                session.execute(Face.insert().values(
+                    frame_id=frame_id,
+                    VIP_id=VIP_id,
+                    bounding_box=bounding_box
+                ))
+
 
     # commit db changes
     session.commit()
-
-
+    print("[debug] total process takes {}".format(datetime.now() - tick))
+    count += 1
+    print("[debug] received {} images".format(count))
     # image_hub.send_reply(b'OK')
+
--- utils/utils.py
+++ utils/utils.py
@@ -28,15 +28,15 @@ def get_VIPs(session, embedding_model, vip_model):
     vip_embeds = np.array(vip_embeds)
     return vip_ids, vip_embeds
 
-def crop(image, box, padding=None):
+def crop(image, box, padding=5):
     height, width = image.shape[:2]
     x0, y0, w, h = box
     x1, y1 = x0 + w, y0 + h
     if padding:
         size = max(x1 - x0, y1 - y0)
         xm, ym = (x0 + x1) / 2, (y0 + y1) / 2
-        x0, y0 = xm - size / 2 - 5, ym - size / 2 - 5
-        x1, y1 = xm + size / 2 + 5, ym + size / 2 + 5
+        x0, y0 = xm - size / 2 - padding, ym - size / 2 - padding
+        x1, y1 = xm + size / 2 + padding, ym + size / 2 + padding
     x0, y0, x1, y1 = list(map(int, [x0, y0, x1, y1]))
     return image[max(0, y0): min(y1, height), max(0, x0): min(x1, width)]
 
